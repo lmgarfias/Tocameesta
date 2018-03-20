2f16b251fc056a8a7e63e8d85326369731edd810
luisgarfias <lmgarfias15@gmail.com>
2018-03-19 23:23:21 -0700

    Added questions in string xml and created the sendResultsWithEmail method was created.

--- app/src/main/java/com/example/luisgarfias/project3/MainActivity.java
+++ app/src/main/java/com/example/luisgarfias/project3/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.luisgarfias.project3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+public void sendEmailWithResults(View view) {
+
+    CheckBox emailCheckBox = (CheckBox) findViewById(R.id.email_results);
+    boolean emailResults = emailCheckBox.isChecked();
+
 }
+
+}
+
+
